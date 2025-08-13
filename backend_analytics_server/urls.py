0c9a9d51608e77a103884a6ec298e2ed5b058503
Cliente CONTROL-PC <CltControl@GGIECLUGWRK170.espol.edu.ec>
2025-08-13 10:08:37 -0500

    Guía 24 Completada

--- backend_analytics_server/urls.py
+++ backend_analytics_server/urls.py
@@ -21,10 +21,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("dashboard/", include("dashboard.urls")),
-    path("", include("dashboard.urls")),
     # Ruta login/ para la vista LoginView para inicio de sesión, uso de plantilla y alias
     path('login/', auth_views.LoginView.as_view(template_name='security/login.html'), name='login'),
     # Ruta logout/ para la vista LogoutView para fin de sesión, redirección y alias
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+    path("dashboard/", include("dashboard.urls")),
+    path("", include("dashboard.urls")),    
 ]
